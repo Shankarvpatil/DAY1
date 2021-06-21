c885c29dd7da03db90e110b6b68e8ff0e1e898ae
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-21 15:10:42 +0530

    Update Presentornot.java

--- employeepresent/Presentornot.java
+++ employeepresent/Presentornot.java
@@ -1,11 +1,29 @@
-public class Presentornot {
-        public static void main(String[] args) {
-                int Full_time = 1;
-                double empCheck = Math.floor(Math.random() * 10) % 2;
-                if(empCheck == Full_time)
-                        System.out.println("Employee is present");
-                else
-                        System.out.println("Employee is absent");
-        }
-}
+package uc2_Calculate_Daily_Employee_Wage;
+import uc1_Emloyee_Attendance.Emp_Attendance;
 
+public class Daily_Wage extends Emp_Attendance {
+	
+	public static int Wager() {
+		
+		int WagePerHr = 20, FullDayHr = 8;
+		int Daily_wage = WagePerHr * FullDayHr;
+		return Daily_wage;
+	}
+	
+	public static void main(String[] args) {
+		
+		int attendance = Attendance();
+		
+		int isPresent = 1, isAbsent = 0; 
+		
+		if (attendance == isPresent) {
+			
+		System.out.print("Employee Daily wager = "+Wager());
+		}
+		
+		else {
+			
+			System.out.println("Employee Daily Wager = "+isAbsent);
+		}
+	}
+}
