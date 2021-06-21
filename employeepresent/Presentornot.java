7c426555a63254d1a4e8c9676a5fc47b2c3a5d03
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-21 16:18:36 +0530

    Update Presentornot.java

--- employeepresent/Presentornot.java
+++ employeepresent/Presentornot.java
@@ -1,35 +1,41 @@
-package UC5_Employee_Month_Wages;
+package UC6_Wages_tillCondiytion_Total_working;
 
 import UC4_Using_SwitchCase.SwitchCase;
 
-public class Monthly_Wages extends SwitchCase {
+public class Montly_Wage extends SwitchCase {
 
 	public static void main(String[] args) {
 		
-		int TotalWage = 0;
+		int TotalWage = 0, day = 0, WorkingHour = 0;
 		
-		for(int Day = 1; Day <= 20; Day++) {
+		while( day <= 20 || WorkingHour <= 100 ) {
 		
 			int check = Switch();
 			
 			switch (check) {
 			
 			case 1:                                                     
-				TotalWage += Wage();     
+				
+				day++;
+				WorkingHour += 8;
+				TotalWage += Wager();    
 				break;
 				
-			case 2:                                                      
-			
+			case 2:                                                 
+				
+				day++;
+				WorkingHour += 4;
 				TotalWage += PartTimeWage() + Wage();
 				break;
 				
-			default:                                                   
-
-               TotalWage += 0;
+			default: 
+				day++;
+				WorkingHour += 0;
+                TotalWage += 0;
 			}
 		}
 
-		System.out.println(" Employee Monthly wager = "+TotalWage);
+		System.out.println(" Employee Monthly wage = "+TotalWage);
 	}
 
 }
