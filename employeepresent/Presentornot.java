e9d50389ab3a250b74187d9f0f9e1ef8b65098c3
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-22 07:56:05 +0530

    Update Presentornot.java

--- employeepresent/Presentornot.java
+++ employeepresent/Presentornot.java
@@ -1,49 +1,30 @@
-package UC3_Part_time_Employee_Wages;
-
-import java.util.Random;
-
-import UC2_Daily_Wage.Daily_Wage;
-
-public class PartTime_Wage extends Daily_Wage {   
-	
-	public static int Attendance() {
-		
-		Random random = new Random();
-		int check = random.nextInt(3);
-		
-		return check;
-	}
-	
-    public static int PartTimeWage() {           
-    	
-    	int Wage = 20, PartTime = 4;
-    	int PartTimeWage = Wage * PartTime;
-        return PartTimeWage;
-	
-    	
-    }
-    
-	public static void main(String[] args) {
-		
-		int isPresent = 1, isParttime = 2, isAbsent=0;
-		
-		int attendance = Attendance();  
-		int dailyWage = dailyWage();                    
-		
-		if (attendance == isPresent) {
-			
-			System.out.print("Employee Full day wage = "+dailyWage);
-			}
-			
-		else if(attendance == isParttime) {
-
-			System.out.println("Employee Part time wage = "+PartTimeWage());
-			}
-		
-		else {
-			
-			System.out.println("Employee Daily Wager = "+isAbsent);
+public class EmployeeWage
+{
+	public static void main(String args[])
+	{
+		int is_fulltime_present = 1;
+		int is_parttime_present = 2;
+		int daily_wage;
+		int wage_per_hr = 20;
+		int working_hr;
+		int emp_check = (int)(Math.floor(Math.random() * 10)) % 3;
+		if( emp_check == is_fulltime_present)
+		{
+			System.out.println("Employee is full time present");
+			working_hr = 8; 
 		}
+		else if( emp_check == is_parttime_present)
+		{
+			System.out.println("Employee is part time present");
+			working_hr = 4;
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+			working_hr = 0;
+		}
+		
+		daily_wage = working_hr * wage_per_hr;
+		System.out.println("Employee daily wage is " + daily_wage);
 	}
-
 }
