5ed8fdafc8175191d58f0378f769bbab03b54009
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-22 07:56:50 +0530

    Update Presentornot.java

--- employeepresent/Presentornot.java
+++ employeepresent/Presentornot.java
@@ -1,33 +1,27 @@
-package UC4_Using_SwitchCase;
-
-import java.util.*;
-
-import UC3_PartTime_Emp_Wage.PartTime_Wage;
-public class Switch extends PartTime_Wage {
-	
-	public static int Switchcase() {
-		
-		Random randome = new Random();
-		int ans = random.nextInt(3);
-		return ans;
-	}
-
-	public static void main(String[] args) {
-		
-		int ans = Switch();
-		
-		switchcase (ans) {
-		
-		case 1:                                                       
-			System.out.println("Employee Full day Wager = "+Wage());
-			break;
-			
-		case 2:                                                    
-		
-			System.out.println("Employee Part time Wager = "+PartTimeWage());
-			break;
-		default:
-			System.out.println("Employee is Absent");             
+public class EmployeeWage
+{
+	public static void main(String args[])
+	{
+		int is_fulltime_present = 1;
+		int is_parttime_present = 2;
+		int daily_wage;
+		int wage_per_hr = 20;
+		int working_hr=0;
+		int emp_check = (int)(Math.floor(Math.random() * 10)) % 3;
+		switch (emp_check) {
+			case 1:
+				System.out.println("Employee is full time present");
+				working_hr = 8;
+				break;
+			case 2:
+				System.out.println("Employee is part time present");
+				working_hr = 4;
+				break;
+			default:
+				System.out.println("Employee is absent");
+				working_hr = 0;
 		}
-		
+		daily_wage = working_hr * wage_per_hr;
+		System.out.println("Employee daily wage is " + daily_wage);
 	}
+}
